6f2cac33d008345541be07c84315ed80cdbf6af5
José Antonio Delgado <jose1984@gmail.com>
2018-11-16 10:06:25 +0100

    adapt implementation to war packaging

--- src/main/kotlin/es/jadel/web/vuejs/VueWorkshopApplication.kt
+++ src/main/kotlin/es/jadel/web/vuejs/VueWorkshopApplication.kt
@@ -1,12 +1,20 @@
 package es.jadel.web.vuejs
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class VueWorkshopApplication
+class VueWorkshopApplication : SpringBootServletInitializer() {
 
-fun main(args: Array<String>) {
-    runApplication<VueWorkshopApplication>(*args)
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder =
+            application.sources(VueWorkshopApplication::class.java)
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            runApplication<VueWorkshopApplication>(*args)
+        }
+    }
+
 }
-
